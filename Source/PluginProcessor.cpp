26cb512add6a47831e8de46d248342783830da09
xenakios <xenakios@gmail.com>
2024-10-07 23:26:59 +0300

    Cleaning up

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -14,20 +14,14 @@
 
 //==============================================================================
 EvilPluginAudioProcessor::EvilPluginAudioProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
+
     : AudioProcessor(BusesProperties()
-#if !JucePlugin_IsMidiEffect
-#if !JucePlugin_IsSynth
                          .withInput("Input", AudioChannelSet::stereo(), true)
-#endif
                          .withOutput("Output", AudioChannelSet::stereo(), true)
-#endif
+
       )
-#endif
+
 {
-    // static int count = 0;
-    // m_osc_frequency += (double)count*10.0;
-    //++count;
 }
 
 EvilPluginAudioProcessor::~EvilPluginAudioProcessor() {}
@@ -44,23 +38,9 @@ bool EvilPluginAudioProcessor::acceptsMidi() const
 #endif
 }
 
-bool EvilPluginAudioProcessor::producesMidi() const
-{
-#if JucePlugin_ProducesMidiOutput
-    return true;
-#else
-    return false;
-#endif
-}
+bool EvilPluginAudioProcessor::producesMidi() const { return false; }
 
-bool EvilPluginAudioProcessor::isMidiEffect() const
-{
-#if JucePlugin_IsMidiEffect
-    return true;
-#else
-    return false;
-#endif
-}
+bool EvilPluginAudioProcessor::isMidiEffect() const { return false; }
 
 double EvilPluginAudioProcessor::getTailLengthSeconds() const { return 0.0; }
 
@@ -87,35 +67,21 @@ void EvilPluginAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBl
     g_osc_phase = 0.0;
 }
 
-void EvilPluginAudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
+void EvilPluginAudioProcessor::releaseResources() {}
 
-#ifndef JucePlugin_PreferredChannelConfigurations
 bool EvilPluginAudioProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const
 {
-#if JucePlugin_IsMidiEffect
-    ignoreUnused(layouts);
-    return true;
-#else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
     if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono() &&
         layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
         return false;
 
-        // This checks if the input layout matches the output layout
-#if !JucePlugin_IsSynth
+    // This checks if the input layout matches the output layout
+
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-#endif
 
     return true;
-#endif
 }
-#endif
 
 void EvilPluginAudioProcessor::processBlock(AudioBuffer<float> &buffer, MidiBuffer &midiMessages)
 {
@@ -129,12 +95,13 @@ void EvilPluginAudioProcessor::processBlock(AudioBuffer<float> &buffer, MidiBuff
     double *oscphasevariable = &m_osc_phase;
     if (m_use_global_variable)
         oscphasevariable = &g_osc_phase;
+    auto bufptrs = buffer.getArrayOfWritePointers();
     for (int i = 0; i < buffer.getNumSamples(); ++i)
     {
         float s = 0.02 * sin(2 * juce::MathConstants<double>::pi / getSampleRate() *
                              (*oscphasevariable) * m_osc_frequency);
         for (int j = 0; j < buffer.getNumChannels(); ++j)
-            buffer.setSample(j, i, s);
+            bufptrs[j][i] = s;
         (*oscphasevariable) += 1.0;
     }
     if (m_sleep_in_audio_thread)
--- Source/xen_utilities.h
+++ Source/xen_utilities.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "../JuceLibraryCode/JuceHeader.h"
 #include <algorithm>
+#include <random>
 
 template<typename Cont, typename F>
 inline void erase(Cont& c, F f)
